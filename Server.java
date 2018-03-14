86b2395bac80c520bce3ffca76f852c257248256
Petros <psdeme@hotmail.com>
2018-03-14 21:23:19 +0200

    initial commit

---
+++ Server.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+ 
+public class Server {
+ 
+    public static void main(String args[]) {
+        new Server().openServer();
+    }
+   
+    /* Define the socket that receives requests */
+	ServerSocket providerSocket;
+	Socket connection = null;
+	
+ 
+    /* Define the socket that is used to handle the connection */
+   
+    void openServer() {
+        try {
+ 
+            /* Create Server Socket */
+			providerSocket = new ServerSocket(4321,10);//mexri 10 exyphretei mpainei sto initial buffer 
+			
+            while (true) {
+                /* Accept the connection */
+				connection = providerSocket.accept();
+                System.out.println("Got a new connection...");
+
+
+
+                
+                /* Handle the request */
+				Thread t = new ActionsForClients(connection);
+				t.start();
+            }
+ 
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            try {
+                providerSocket.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }  
+}
