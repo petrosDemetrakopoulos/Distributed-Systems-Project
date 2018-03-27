53fae97e14cc49a7e09e8266aafa448664eaac98
GeorgeChatzo <23149855+GeorgeChatzo@users.noreply.github.com>
2018-03-27 17:00:24 +0300

    Add files via upload

--- MasterClass.java
+++ MasterClass.java
@@ -1,4 +1,6 @@
-package Project;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.OpenMapRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
 
 import java.io.*;
 import java.net.*;
@@ -7,8 +9,16 @@ import java.util.*;
 public class MasterClass implements Master {
     ServerSocket providerSocket;
     Socket connection = null;
+    private RealMatrix R,P,C;
+    int numberOfConnections=1;
+
+    Map<Object,Object> sourcesCore = new HashMap<Object, Object>();
+    Map<Object,Object> sourcesMemory = new HashMap<Object,Object>();
+    ArrayList<Object> clients = new ArrayList<Object>();
+    Vector<Object> finalRank = new Vector<Object>();
 
     public void initialize() {
+
         try {
             /* Create Server Socket */
             providerSocket = new ServerSocket(10001, 10);//mexri 10 exyphretei mpainei sto initial buffer
@@ -17,11 +27,14 @@ public class MasterClass implements Master {
                 connection = providerSocket.accept();
                 System.out.println("Got a new connection...");
                 /* Handle the request */
-                Thread t = new ActionsForClients(connection);
-                t.start();
+                requestHandler(connection);
+                numberOfConnections++;
+
             }
         } catch (IOException ioException) {
             ioException.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         } finally {
             try {
                 providerSocket.close();
@@ -29,6 +42,69 @@ public class MasterClass implements Master {
                 ioException.printStackTrace();
             }
         }
+    }
+
+    public void requestHandler(Socket connection) throws IOException, ClassNotFoundException {
+        ObjectInputStream in = new ObjectInputStream(connection.getInputStream());
+        ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
+        while(true){
+            Object numberOfCores = in.readObject();
+            Object availiableMemory = in.readObject();
+            Object gigamem = availiableMemory;
+            double mem = ((Number) gigamem).doubleValue();
+            System.out.println("Client "+numberOfConnections+" has number of cores: "+ numberOfCores+" and available memory(GB): "+ mem/(1024*1024*1024));
+            Object name = "Client_"+numberOfConnections;
+            clients.add(name);
+            sourcesCore.put(name,numberOfCores);
+            sourcesMemory.put(name,availiableMemory);
+            Rank(sourcesCore,sourcesMemory);
+        }
+    }
+
+    public void Rank(Map<Object, Object> sourceCPU, Map<Object, Object> sourceMEM){
+
+        Map<Object, Object> map1 = sortByValues(sourceCPU);
+        Map<Object, Object> map2 = sortByValues(sourceMEM);
+
+        //CPU SORTING
+        Set set1 = map1.entrySet();
+        Iterator iterator1 = set1.iterator();
+        while(iterator1.hasNext()) {
+            Map.Entry cpu = (Map.Entry)iterator1.next();
+            System.out.print(cpu.getKey() + ": ");
+            System.out.println(cpu.getValue());
+        }
+
+        //MEMORY SORTING
+        Set set2 = map2.entrySet();
+        Iterator iterator2 = set2.iterator();
+        while(iterator2.hasNext()) {
+            Map.Entry memory = (Map.Entry)iterator2.next();
+            System.out.print(memory.getKey() + ": ");
+            System.out.println(memory.getValue());
+        }
+
+       // List<Object> indexes1 = new ArrayList<Object>(map1.keySet());
+       // List<Object> indexes2 = new ArrayList<Object>(map2.keySet());
+
+    }
+
+    //overided comperator
+    private static HashMap sortByValues(Map<Object, Object> map) {
+        List list = new LinkedList(map.entrySet());
+        Collections.sort(list, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o1)).getValue())
+                        .compareTo(((Map.Entry) (o2)).getValue());
+            }
+        });
+        //Copying the sorted list
+        HashMap sortedHashMap = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext();) {
+            Map.Entry entry = (Map.Entry) it.next();
+            sortedHashMap.put(entry.getKey(), entry.getValue());
+        }
+        return sortedHashMap;
     }
 
     public static void main(String args[]){
--- WorkerClass.java
+++ WorkerClass.java
@@ -1,12 +1,17 @@
-package Project;
+import org.apache.commons.math3.analysis.function.Inverse;
+import org.apache.commons.math3.linear.*;
 
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
 public class WorkerClass extends Thread implements Worker  {
     private int availableProcessors;
     private long availableMemory;
+    private static OpenMapRealMatrix sparse_m = new OpenMapRealMatrix(200,200);
+    private static RealMatrix Cmatrix = MatrixUtils.createRealMatrix(200,200);
+    private static RealMatrix X = MatrixUtils.createRealMatrix(200,20);
+    private static RealMatrix Y = MatrixUtils.createRealMatrix(200,20);
+    private RealMatrix Cu,Ci;
 
     public WorkerClass(int availableProcessors,long availableMemory){
         this.availableProcessors = availableProcessors;
@@ -16,10 +21,25 @@ public class WorkerClass extends Thread implements Worker  {
     public WorkerClass(){}
 
     public void initialize(){
-        new WorkerClass(Runtime.getRuntime().availableProcessors(),Runtime.getRuntime().freeMemory()).start();
+        //new WorkerClass(Runtime.getRuntime().availableProcessors(),Runtime.getRuntime().freeMemory()).start();
+        calculateCMatrix(sparse_m);
+
+        for(int i=0; i < 200; i++){
+            for(int j=0; j < 20; j++){
+                X.setEntry(i,j,sparse_m.getEntry(i,j));
+            }
+        }
+
+
+        for(int j=0; j < 200; j++){
+            for(int i=0; i < 20; i++){
+                Y.setEntry(j,i,sparse_m.getEntry(j,i));
+            }
+        }
+
     }
 
-    public void run(){
+    /*public void run(){
         Socket requestSocket = null;
         ObjectInputStream in = null;
         ObjectOutputStream out = null;
@@ -49,7 +69,7 @@ public class WorkerClass extends Thread implements Worker  {
                 ioException.printStackTrace();
             }
         }
-    }
+    }*/
 
     public int getAvailableProcessors(){
         return this.availableProcessors;
@@ -59,7 +79,106 @@ public class WorkerClass extends Thread implements Worker  {
         return this.availableMemory;
     }
 
+    public RealMatrix getCu() {
+        return Cu;
+    }
+
+    public RealMatrix getCi() {
+        return Ci;
+    }
+
+    public void calculateCMatrix(RealMatrix realMatrix){
+
+        int a = 40;
+        for(int i=0; i < 200; i++){
+            for(int j=0; j < 200; j++){
+                Cmatrix.setEntry(i,j, 1 + a* sparse_m.getEntry(i,j));
+            }
+        }
+    }
+
+    public RealMatrix preCalculateXX(RealMatrix realMatrix) {
+        RealMatrix Xtranspose = realMatrix.transpose();
+        return Xtranspose.multiply(realMatrix);
+    }
+
+    public RealMatrix preCalculateYY(RealMatrix realMatrix) {
+        RealMatrix Ytranspose = realMatrix.transpose();
+        return Ytranspose.multiply(realMatrix);
+    }
+
+    //diagwnios pinakas diastasewn nxn (osa einai ta items) pou sthn diagwnio exei tis protimiseis tou xrhsth u
+    public void calculateCuMatrix(int user,RealMatrix realMatrix){
+        double[] diag_elements = realMatrix.getRow(user);
+        Cu = MatrixUtils.createRealDiagonalMatrix(diag_elements);
+    }
+
+    //diagwnios pinakas diastasewn mxm(osoi einai oi users) pou sthn diagwnio exei tis protimiseis olwn twn users gia to item i
+    public void calculateCiMatrix(int item,RealMatrix realMatrix){
+        double[] diag_elements = realMatrix.getColumn(item);
+        Ci = MatrixUtils.createRealDiagonalMatrix(diag_elements);
+    }
+
+    public RealMatrix calculate_x_u(int user, RealMatrix realMatrixYY, RealMatrix realMatrixCu){
+        //TO x_u EINAI GIA KA8E XRHSTH u!!!
+        double l = 0.01;
+        RealMatrix Ytranspose = Y.transpose();
+        RealMatrix product1 = Ytranspose.multiply(realMatrixCu);
+        RealMatrix product2 = product1.multiply(Y);
+        RealMatrix IdentityMatrix = MatrixUtils.createRealIdentityMatrix(realMatrixCu.getColumnDimension());//ftiaxnei monadiaio pinaka
+        RealMatrix regularization = IdentityMatrix.scalarMultiply(l);
+        RealMatrix inverseTerm = product2.add(regularization);
+        RealMatrix Inverse = new QRDecomposition(inverseTerm).getSolver().getInverse();
+        RealMatrix multiplication2 = Ytranspose.multiply(realMatrixCu);
+        double[] pu_data = sparse_m.getRow(user);
+        RealMatrix pu = MatrixUtils.createColumnRealMatrix(pu_data);//ftiaxnei to p(i)
+        RealMatrix multplication3 = multiplication2.multiply(pu);
+        RealMatrix x_u = Inverse.multiply(multplication3);
+
+        return x_u;
+    }
+
+
+    public RealMatrix calculate_y_i(int item, RealMatrix realMatrixXX, RealMatrix realMatrixCi){
+        double l = 0.01;
+        RealMatrix Xtranspose = X.transpose();
+        RealMatrix IdentityMatrix = MatrixUtils.createRealIdentityMatrix(realMatrixCi.getColumnDimension());//ftiaxnei monadiaio pinaka
+        RealMatrix subtract = realMatrixCi.subtract(IdentityMatrix);//afairesh C(i) - I
+        RealMatrix regularization = IdentityMatrix.scalarMultiply(l);//stoixeio pros stoixeio me to l->( l * I)
+        RealMatrix product1 = Xtranspose.multiply(subtract);//X^T * (C(i) - I)
+        RealMatrix product2 = product1.multiply(X);//(X^T * (C(i) - I)) * X
+        RealMatrix InverseTerm1 = realMatrixXX.add(product2);//X^T*X + (X^T * (C(i) - I)) * X
+        RealMatrix InverseTerm = InverseTerm1.add(regularization);//X^T*X + (X^T * (C(i) - I)) * X +l*I
+        RealMatrix Inverse = new QRDecomposition(InverseTerm).getSolver().getInverse();
+        double[] pi_data = sparse_m.getColumn(item);
+        RealMatrix pi = MatrixUtils.createColumnRealMatrix(pi_data);//ftiaxnei to p(i)
+        RealMatrix product3 = Xtranspose.multiply(realMatrixCi);
+        RealMatrix product4 = product3.multiply(pi);
+        RealMatrix y_i = Inverse.multiply(product4);
+
+        return y_i;
+    }
+
     public static void main(String args[]){
-        new WorkerClass().initialize();
+        WorkerClass worker = new WorkerClass();
+        worker.initialize();
+        //for 10 sweeps
+        for(int sweep = 0; sweep < 10; sweep++){
+            //first we will compute all the user factors!!
+            RealMatrix YY = worker.preCalculateYY(Y).copy();//we compute Y^T * Y
+            //for each user
+            for(int user = 0; user < sparse_m.getRowDimension(); user++){
+                worker.calculateCuMatrix(user,Cmatrix);
+                System.out.println(YY.getRowDimension());
+                X.setRowMatrix(user,worker.calculate_x_u(user,YY,worker.getCu()));
+            }
+            //we will compute all the item factors!!
+            RealMatrix XX = worker.preCalculateXX(X).copy();//we compute X^T * X
+            //for each item
+            for(int item = 0; item < sparse_m.getColumnDimension(); item++){
+                worker.calculateCiMatrix(item,Cmatrix);
+                Y.setColumnMatrix(item,worker.calculate_y_i(item,XX,worker.getCi()));
+            }
+        }
     }
 }
